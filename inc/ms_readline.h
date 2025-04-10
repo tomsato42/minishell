f13a480f668b3ab09d1df17c158294ceacfd1439
Tetsuro Ando <86369575+TetsuroAndo@users.noreply.github.com>
2025-04-10 23:13:36 +0900

    Solving the Circulation-Inclusion Problem (#9)

--- inc/ms_readline.h
+++ inc/ms_readline.h
@@ -1,11 +1,9 @@
 #ifndef MS_READLINE_H
 # define MS_READLINE_H
 
-# include "core.h"
+# include <stdio.h>
 # include <readline/history.h>
 # include <readline/readline.h>
-
-// extern volatile sig_atomic_t g_signal_status;
 
 char	*launch_readline(const char *prompt);
 
