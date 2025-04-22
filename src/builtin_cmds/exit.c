2b79be3e31f0fb5fa3624c6c63a275d6ef0291a5
Tetsuro Ando <86369575+TetsuroAndo@users.noreply.github.com>
2025-04-22 13:14:37 +0900

    Hotfix exit (#31)
    * fix: exit err message and status code
    
    * fix: norm
    
    * fix: norm
    
    * fix: Change processing order of stderr output

--- src/builtin_cmds/exit.c
+++ src/builtin_cmds/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomsato <tomsato@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 22:43:58 by teando            #+#    #+#             */
-/*   Updated: 2025/04/21 21:56:45 by tomsato          ###   ########.fr       */
+/*   Updated: 2025/04/22 10:56:09 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,27 @@
 
 t_status	__exit(int argc, char **argv, t_shell *sh)
 {
-	long	code;
-	char	*end;
+	long long	code;
+	char		*end;
 
 	code = sh->status;
-	if (argc > 2)
-		return (ft_dprintf(2, "minishell: exit: too many arguments\n"), 1);
-	if (argc == 2)
-	{
-		code = ft_strtol(argv[1], &end, 10);
-		if (end != argv[1] + ft_strlen(argv[1]) || code > 255 || code < 0)
-		{
-			ft_dprintf(2, "minishell: exit: %s: numeric argument required\n", argv[1]);
-			code = 255;
-		}
-	}
 	if (sh->interactive)
-		printf("exit\n");
+		ft_dprintf(STDERR_FILENO, "exit\n");
+	if (argc >= 2)
+	{
+		errno = 0;
+		code = ft_strtoll(argv[1], &end, 10);
+		if (end != argv[1] + ft_strlen(argv[1]) || errno == ERANGE)
+		{
+			ft_dprintf(2, "minishell: exit: %s: numeric argument required\n",
+				argv[1]);
+			code = E_NUMERIC;
+		}
+		else if (argc > 2)
+			return (ft_dprintf(2, "minishell: exit: too many arguments\n"), 1);
+		else if (code > 255 || code < 0)
+			code = code % 256;
+	}
 	shell_exit(sh, (int)(code & 0xFF));
 	return (42);
 }
