a6c28a2e7254ea270c1471de4911d1a75e28a318
Tomoki Sato <60466946+tomsato42@users.noreply.github.com>
2025-04-23 17:25:47 +0900

    fix: fix args (#39)

--- src/modules/analyze_lexical/validate_redirect_missing_arg.c
+++ src/modules/analyze_lexical/validate_redirect_missing_arg.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validate_redirect_missing_arg.c                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: tomsato <tomsato@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 14:22:01 by teando            #+#    #+#             */
-/*   Updated: 2025/04/22 14:51:27 by teando           ###   ########.fr       */
+/*   Updated: 2025/04/23 17:22:19 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,16 @@ int	validate_redirect_missing_arg(const char *line, size_t *pos)
 {
 	t_token_type	op;
 	size_t			temp_pos;
+	size_t			len;
 
-	op = get_two_char_op(&line[*pos], NULL);
+	op = get_two_char_op(&line[*pos], &len);
 	if (op == TT_ERROR)
 		op = get_one_char_op(line[*pos]);
 	if (op != TT_REDIR_IN && op != TT_REDIR_OUT && op != TT_APPEND
 		&& op != TT_HEREDOC)
-		return (0);
+			return (0);
 	if (op == TT_APPEND || op == TT_HEREDOC)
-		temp_pos = *pos + 2;
+			temp_pos = *pos + 2;
 	else
 		temp_pos = *pos + 1;
 	skip_spaces(line, &temp_pos);
