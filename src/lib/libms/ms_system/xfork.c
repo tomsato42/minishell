445ebf29a3446e2503677a3daffda5e69ac21bdc
Tetsuro Ando <86369575+TetsuroAndo@users.noreply.github.com>
2025-04-29 11:51:52 +0900

    retry (#51)

--- src/lib/libms/ms_system/xfork.c
+++ src/lib/libms/ms_system/xfork.c
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 00:58:56 by teando            #+#    #+#             */
-/*   Updated: 2025/04/11 00:59:16 by teando           ###   ########.fr       */
+/*   Updated: 2025/04/28 21:12:05 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,18 @@
 pid_t	xfork(t_shell *shell)
 {
 	pid_t	pid;
+	int		retry;
 
 	pid = fork();
 	if (pid == -1)
 	{
+		retry = 0;
+		while (retry++ < 5)
+		{
+			pid = fork();
+			if (pid != -1)
+				return (pid);
+		}
 		perror("fork");
 		shell_exit(shell, errno);
 	}
